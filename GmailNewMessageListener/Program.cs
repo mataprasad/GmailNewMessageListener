c61f42e18b5cd815860e76af56a23199a34acc68
mata <mata@purplestylelabs.com>
2021-10-10 12:49:40 +0530

    gmail new message listen

---
+++ GmailNewMessageListener/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using MailKit.Security;
+
+namespace GmailNewMessageListener
+{
+	class Program
+	{
+		// Connection-related properties
+		public const SecureSocketOptions SslOptions = SecureSocketOptions.Auto;
+		public const string Host = "imap.gmail.com";
+		public const int Port = 993;
+
+		// Authentication-related properties
+		public const string Username = "dev.manntalks@gmail.com";
+		public static string Password = string.Empty;
+		static Program()
+        {
+			dotenv.net.DotEnv.Load();
+			Password = Environment.GetEnvironmentVariable("DevMannPassword");
+		}
+		public static void Main(string[] args)
+		{
+			using (var client = new IdleClient())
+			{
+				Console.WriteLine("Hit any key to end the demo.");
+
+				var idleTask = client.RunAsync();
+
+				Task.Run(() => {
+					Console.ReadKey(true);
+				}).Wait();
+
+				client.Exit();
+
+				idleTask.GetAwaiter().GetResult();
+			}
+		}
+	}
+}
